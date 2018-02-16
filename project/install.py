54b706c3fc620f2b2bb08b51842f6c87f64c2a9c
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2018-02-16 15:55:33 +0000

    [IMP] project/install.py (ADD clv_medicament_pharm)

--- project/install.py
+++ project/install.py
@@ -511,6 +511,9 @@ def install():
     group_names = []
     install_update_module('clv_global_tag_pharm', update, group_names)
 
+    group_names = []
+    install_update_module('clv_medicament_pharm', update, group_names)
+
 
 def secondsToStr(t):
 
